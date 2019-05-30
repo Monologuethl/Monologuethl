97305d1e0f0673aeb838b4e7782781581bb32016
Monologuethl <2481871325@qq.com>
2019-05-30 14:54:54 +0800

    update

--- code/dicom_process/batch_generator.py
+++ code/dicom_process/batch_generator.py
@@ -6,31 +6,51 @@ import pydicom
 import pylab
 from PIL import Image
 
-WSI_MASK_PATH = 'data'
-paths_png = glob.glob(os.path.join(WSI_MASK_PATH, '*.png'))
-paths_dcm = glob.glob(os.path.join(WSI_MASK_PATH, '*.dcm'))
-paths_png.sort()
-paths_dcm.sort()
-print(paths_png)
-print(paths_dcm)
+# WSI_MASK_PATH = 'data'
+# paths_png = glob.glob(os.path.join(WSI_MASK_PATH, '*.png'))
+# paths_dcm = glob.glob(os.path.join(WSI_MASK_PATH, '*.dcm'))
+# paths_png.sort()
+# paths_dcm.sort()
+# print(paths_png)
+# print(paths_dcm)
 
-j = 0
-for size in range(len(paths_png)):
-    ds = pydicom.read_file(paths_dcm[size])
-    img = cv2.imread(paths_png[size])
-    Grayimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(Grayimg, 30, 255, cv2.THRESH_BINARY)
-    cv2.imwrite("data/" + str(j) + ".jpg", thresh, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
-    img = Image.open("data/" + str(j) + ".jpg")
-    matrix = np.asarray(img)
-    arr = matrix.flatten()
-    for i in range(262144):
-        if arr[i] < 100:
+ds = pydicom.read_file(r"D:\TONG\github\Monologuethl\data\I160.dcm")
+
+ds.PixelData = ds.pixel_array.tostring()
+ds.save_as("data/ test.dcm")
+
+for i in range(262144):
+
+        if 100 > ds.pixel_array.flat[i] > 250:
+            print("")
             ds.pixel_array.flat[i] = 0
-    ds.PixelData = ds.pixel_array.tostring()
-    ds.save_as("data/" + str(j) + ".dcm")
-    j = j + 1
-    print(j)
+            print(ds.pixel_array.flat[i])
+
+
+np_ds = ds.pixel_array
+
+pylab.imshow(np_ds, cmap=pylab.cm.bone)
+
+pylab.show()
+
+#
+# j = 0
+# for size in range(len(paths_png)):
+#     ds = pydicom.read_file(paths_dcm[size])
+#     img = cv2.imread(paths_png[size])
+#     Grayimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#     ret, thresh = cv2.threshold(Grayimg, 30, 255, cv2.THRESH_BINARY)
+#     cv2.imwrite("data/" + str(j) + ".jpg", thresh, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
+#     img = Image.open("data/" + str(j) + ".jpg")
+#     matrix = np.asarray(img)
+#     arr = matrix.flatten()
+#     for i in range(262144):
+#         if arr[i] < 100:
+#             ds.pixel_array.flat[i] = 0
+#     ds.PixelData = ds.pixel_array.tostring()
+#     ds.save_as("data/" + str(j) + ".dcm")
+#     j = j + 1
+#     print(j)i'ho'f'j'j
 
 # pylab.imshow(ds.pixel_array, cmap=pylab.cm.bone)
 # pylab.show()
